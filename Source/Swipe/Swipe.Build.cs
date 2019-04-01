4c992ee7a75d67440a272ddc6fdd1c2bcc105f82
Zharay <danvargas404@gmail.com>
2019-04-01 10:47:34 -0700

    Updated to 4.18+  * Only needed to update the build.cs file

--- Source/Swipe/Swipe.Build.cs
+++ Source/Swipe/Swipe.Build.cs
@@ -3,55 +3,61 @@
 //  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
 //
 
-namespace UnrealBuildTool.Rules
+using UnrealBuildTool;
+
+public class Swipe : ModuleRules
 {
-	public class Swipe : ModuleRules
-	{
-		public Swipe(ReadOnlyTargetRules Target) : base(Target)
-		{
-			PublicIncludePaths.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-				}
-				);
+	public Swipe(ReadOnlyTargetRules Target) : base(Target)
+    {
 
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					"Developer/Swipe/Private",
-					// ... add other private include paths required here ...
-				}
-				);
+        bEnforceIWYU = false;
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"Core",
-					"CoreUObject",
-					"Engine"
-					// ... add other public dependencies that you statically link with here ...
-				}
-				);
+        PublicIncludePaths.AddRange(
+			new string[] {
+                "Swipe/Public",
+                "Swipe/Classes"
+				// ... add public include paths required here ...
+			}
+			);
 
-			PrivateIncludePathModuleNames.AddRange(
-				new string[] {
-					"Settings"
-				}
-				);
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"Swipe/Private",
+				// ... add other private include paths required here ...
+			}
+			);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine"
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+
+		PrivateIncludePathModuleNames.AddRange(
+			new string[] {
+				"Settings"
+			}
+			);
 
 
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-					// ... add private dependencies that you statically link with here ...
-				}
-				);
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...
+			}
+			);
 
-			DynamicallyLoadedModuleNames.AddRange(
-				new string[]
-				{
-					// ... add any modules that your module loads dynamically here ...
-				}
-				);
-		}
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
 	}
 }
+
